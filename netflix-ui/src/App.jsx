3ff7c0f60d09a026aeb563e6dfd8f8775bc1331c
Sahil Singh <200104050@hbtu.ac.in>
2023-08-16 11:27:53 +0530

    commit

--- netflix-ui/src/App.jsx
+++ netflix-ui/src/App.jsx
@@ -6,7 +6,7 @@ import Netflix from './pages/Netflix'
 import Player from './pages/Player'
 import Movies from './pages/Movies'
 import TVShow from './pages/TVShow'
-import UserLiked from './pages/UserLiked'
+// import UserLiked from './pages/UserLiked'
 
 export default function App() {
   return (
@@ -17,7 +17,7 @@ export default function App() {
         <Route exact path='/player' element={<Player/>}/>
         <Route exact path='/movies' element={<Movies/>}/>
         <Route exact path='/tv' element={<TVShow/>}/>
-        <Route exact path='/mylist' element={<UserLiked/>}/>
+        {/* <Route exact path='/mylist' element={<UserLiked/>}/> */}
         <Route exact path='/' element={<Netflix/>}/>
       </Routes>
     </BrowserRouter>
